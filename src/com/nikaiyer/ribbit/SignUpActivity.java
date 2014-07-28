2563a54f20e3d1254e2354fbb952056ed6fcdf85
NikAiyer <nick.aiyer@gmail.com>
2014-07-28 14:24:31 -0700

    Created a repo

---
+++ src/com/nikaiyer/ribbit/SignUpActivity.java
@@ -0,0 +1,30 @@
+package com.nikaiyer.ribbit;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class SignUpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sign_up);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.sign_up, menu);
+		return true;
+	}
+}
